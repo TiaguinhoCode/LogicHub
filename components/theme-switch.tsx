058011e45bac5b7f288ee48cc65cee580e1cdbd8
unknown <tiago.rafael019@gmail.com>
2025-03-19 00:45:51 -0300

    Ladding page finalizada

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -7,8 +7,6 @@ import { useTheme } from "next-themes";
 import { useIsSSR } from "@react-aria/ssr";
 import clsx from "clsx";
 
-import { SunFilledIcon, MoonFilledIcon } from "@/components/icons";
-
 export interface ThemeSwitchProps {
   className?: string;
   classNames?: SwitchProps["classNames"];
@@ -70,11 +68,11 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
           ),
         })}
       >
-        {!isSelected || isSSR ? (
+        {/* {!isSelected || isSSR ? (
           <SunFilledIcon size={22} />
         ) : (
           <MoonFilledIcon size={22} />
-        )}
+        )} */}
       </div>
     </Component>
   );
